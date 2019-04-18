3df40f30f2ecb4f2d99ab8042a74822db0ea8a8a
Iman Kalyan Majumdar <iman.m93@gmail.com>
2019-04-18 21:21:56 +0200

    Boiler plate

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          // Add Facebook component here
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
